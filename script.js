7350a59ab787fa36f15504d2e57bd049a90a7f90
Marco <marco.galetta@hotmail.com>
2022-03-29 17:43:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+var ora = document.getElementById('hours').innerHTML;
+var prezzoPerOra = 10;
+var prezzoOraSenzaSconto = prezzoPerOra * ora;
+var scritto = "Il prezzo finale è "
+var errore = "Codice non valido, il prezzo è"
+var euro = "Euro"
+console.log(prezzoOraSenzaSconto);
+
+if ((code == "YHD4U32")) {
+ document.getElementById('discount').innerHTML = scritto + ((prezzoOraSenzaSconto / 100) * 80).toFixed(2) + euro;
+} 
+ else {
+ document.getElementById('discount').innerHTML = errore + (prezzoBigliettoSenzaSconto).toFixed(2) + euro;
+
+}
